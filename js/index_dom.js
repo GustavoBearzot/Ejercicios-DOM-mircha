e652af314b0a8a523429c291993d93f3cd7e6657
Gustavo Bearzot <bearzot@live.com.ar>
2021-07-19 11:15:39 -0300

    falta ejercicio de eventos de teclado

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,3 +1,5 @@
+import scrollTopButton from "./dom/boton_scroll.js";
+import countdown from "./dom/cuenta_regresiva.js";
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
 import { shortcuts } from "./dom/teclado.js";
@@ -7,6 +9,9 @@ d.addEventListener("DOMContentLoaded", (e) => {
   hamburgerMenu(".panel-btn", ".panel");
   digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
   alarm("assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
+  countdown("countdown", "June 16, 2022 07:45:00", "Feliz Cumple Gus");
+  scrollTopButton(".scroll-top-btn"
+  )
 });
 
 d.addEventListener("keydown", (e) => {
