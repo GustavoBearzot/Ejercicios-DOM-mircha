d454ade33ee129917ad5cc6396656d63fd323984
Gustavo Bearzot <bearzot@live.com.ar>
2021-07-21 20:33:32 -0300

    se terminó codigo tema oscuro con localStorage

--- js/index_dom.js
+++ js/index_dom.js
@@ -3,6 +3,7 @@ import countdown from "./dom/cuenta_regresiva.js";
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
 import { shortcuts } from "./dom/teclado.js";
+import { darkTheme } from "./dom/tema_oscuro.js";
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
@@ -10,12 +11,13 @@ d.addEventListener("DOMContentLoaded", (e) => {
   digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
   alarm("assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
   countdown("countdown", "June 16, 2022 07:45:00", "Feliz Cumple Gus");
-  scrollTopButton(".scroll-top-btn"
-  )
+  scrollTopButton(".scroll-top-btn");
 });
 
 d.addEventListener("keydown", (e) => {
   shortcuts(e);
+  //moveBall(e, ".ball", ".stage");
 });
 //"keyup": cuando suelta la tecla.
 //"keypress": mientras mantega presionada la tecla.
+darkTheme(".dark-theme-btn", "dark-mode");
